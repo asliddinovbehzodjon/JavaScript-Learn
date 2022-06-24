7620a3b90b91967b556dd6fd630524f4d90575d5
Behzod Asliddinov <behzodme@gmail.com>
2022-06-24 10:01:29 +0500

    lesson1

---
+++ script.js
@@ -0,0 +1,50 @@
+// console.log('Hello friends')
+// comment(write two slash)
+/* 
+string
+number(integer)
+bool
+null
+array (list)
+object (dict)
+*/
+// Declariotion(elon qilish)
+// assignment (qiymat berish)
+// var a; //declaration
+// console.log(a)
+// a = 5 // assingment 
+// console.log(a)
+// let - ozgaruvchini elon qilishga yordam beradi
+// let a ; //declaration
+// console.log(typeof(a))
+// typeof() - ozgaruvchini qaysi turda ekanligini bildiradi
+// Integer - sonli qiymatlar
+// let a= 5;
+// console.log(typeof(a))
+/*
++ 
+- 
+* 
+/ 
+** (daraja) 2**3 = 2^3
+// - butun bolish 10//3 = 3
+% - qoldiqli bolish 10%3 = 1
+*/
+// const(konstanta) - o'zgarmas
+// const a= 10;
+// let a=6;
+// // 7
+// // a = a+1
+// // console.log(a)
+// // a++ (increment)
+// // console.log(a)
+// a--
+// console.log(a)
+// console.log('I am JS developer')
+// let a=5;
+// let b =8;
+// console.log(a-b)
+
+
+
+
